f7846faaaa745f5661bfcbb64e417f615923d73d
ArturZnosenko <arturznosenko@gmail.com>
2019-02-04 19:48:37 +0200

    Max funkcija sukurta

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite 3 skaicius");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+            int skaicius3 = Convert.ToInt32(Console.ReadLine());
+
+            int maxnum = max(skaicius1, skaicius2, skaicius3);
+            Console.WriteLine(maxnum);
+
+            Console.ReadLine();
+        }
+
+        static int max (int a, int b, int c)
+        {
+            int number = 0;
+            if ((a>b) && (a>c))
+                    {
+                number = a;
+            }
+            else if (b>c)
+            {
+                number = b;
+            }
+            else
+            {
+                number = c;
+            }
+            return number;
         }
     }
 }
